6e033f160f53ea5b5a2828aeabf2bd8229504b21
Daniel Qiang <daniel_qiang@hotmail.com>
2020-09-07 18:03:00 -0700

    add speech synthesis voice enums

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from zoombot.speech_to_text import SpeechToTextStream
 from zoombot.text_to_speech import TextToSpeechStream
 from zoombot.mitsuku import Mitsuku
+from zoombot.enums import Voices
 
 from contextlib import ExitStack
 from textwrap import fill
@@ -8,8 +9,12 @@ from textwrap import fill
 
 def main():
     with ExitStack() as stack:
+        # Female WaveNet, en-US
+        voice = Voices.WaveNet.EN_US_WAVENET_H.value
+
         stt_stream = stack.enter_context(SpeechToTextStream())
-        tts_stream = stack.enter_context(TextToSpeechStream())
+        tts_stream = stack.enter_context(TextToSpeechStream(language_code=voice.language_code,
+                                                            voice_name=voice.name))
         mitsuku = stack.enter_context(Mitsuku())
 
         print('ZoomBot initialized. Ready to go!')
--- zoombot/__init__.py
+++ zoombot/__init__.py
@@ -3,3 +3,4 @@ from .speech_to_text import RecordingStream, SpeechToTextStream
 from .text_to_speech import OutputStream, TextToSpeechStream
 from .mitsuku import Mitsuku
 from .consts import *
+from .enums import Voices
