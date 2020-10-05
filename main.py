9f04ba13f0acabf5e728d91fb39d6a9e73914937
Daniel Qiang <daniel_qiang@hotmail.com>
2020-10-04 23:47:20 -0700

    Fix CLI indentation when response contains nbsp

--- main.py
+++ main.py
@@ -3,9 +3,34 @@ from zoombot.mitsuku import Mitsuku
 from zoombot.consts import Voices
 
 from contextlib import ExitStack
-from textwrap import fill
 
 VOICE = Voices.DEFAULT
+USERNAME = "Daniel"
+BOTNAME = "ZoomBot"
+
+
+def _format_message(user: str, message: str):
+    from textwrap import fill
+
+    # Remove non-breaking space; assumes `message`
+    # only has extended ascii characters
+    # (decimal ascii code < 256)
+    message = message.replace(chr(160), "")
+
+    # Wrap lines, dropping all whitespace except newlines
+    indent = " " * (len(user) + 2)
+    lines = []
+    for i, line in enumerate(message.split("\n")):
+        if i == 0:
+            filled = fill(line, subsequent_indent=indent)
+        else:
+            filled = fill(
+                line, initial_indent=indent, subsequent_indent=indent
+            )
+        lines.append(filled)
+
+    formatted = "\n".join(lines)
+    return f"{user}: {formatted}"
 
 
 def talk():
@@ -14,7 +39,7 @@ def talk():
         tts_stream = stack.enter_context(
             TextToSpeechStream(
                 language_code=VOICE.language_code,
-                voice_name=VOICE.name
+                voice_name=VOICE.name,
             )
         )
         mitsuku = stack.enter_context(Mitsuku())
@@ -23,9 +48,11 @@ def talk():
         print("=" * 40)
 
         for message in stt_stream:
-            print(f'Daniel: {fill(message, subsequent_indent=" " * 8)}')
+            # print(f'Daniel: {fill(message, subsequent_indent=" " * 8)}')
+            print(_format_message(USERNAME, message))
             response = mitsuku.send(message)
-            print(f'ZoomBot: {fill(response, subsequent_indent=" " * 9)}')
+            # print(f'ZoomBot: {fill(response, subsequent_indent=" " * 9)}')
+            print(_format_message(BOTNAME, response))
             tts_stream.write(response)
 
 
@@ -70,10 +97,10 @@ def zoom():
             # and skip it.
             if _sequence_diff(prev_response, message) > 0.85:
                 continue
-            print(f'Daniel: {fill(message, subsequent_indent=" " * 8)}')
+            print(_format_message(USERNAME, message))
             response = mitsuku.send(message)
             prev_response = response
-            print(f'ZoomBot: {fill(response, subsequent_indent=" " * 9)}')
+            print(_format_message(BOTNAME, response))
             tts_stream.write(response)
 
 
